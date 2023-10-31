5172a5085895cd61fff872da1ba7494ea7f9e997
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-10-31 20:04:26 +0000

    Changes to be committed: new file:   01_basics/01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail = "mohit@gmail.com";
+var accountPassword = "12345";
+let accountState;
+accountCity = "Bhopal";
+
+
+// accountId = 111111;
+accountEmail = "jha@gmail.com";
+accountPassword = 67891;
+accountCity = "Indore"
+
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
