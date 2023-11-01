0af812e2923292059331b605d2c8664e6d11f71e
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-11-01 11:07:30 +0000

    Changes to be committed: new file:   01_basics/02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all the JS code as newer version.
+
+// alert(3+3) We are using node.js, not browser.
+
+//Readability 
+
+//console.log(3+3) console.log("Mohit"); // this is  the wrong way to write code they makes errer in our code 
+
+// console.log(3+3);
+
+// console.log("Mohit"); // that is the right and readability way to write code.
+
+let name = "Mohit";
+let age = 18;
+let isloggedIn = false;
+let state = null;
+
+// number => 2 to power 53
+// bigInt => Any big Number data uselly use in trading apps;
+// String => "Mohit" // when we write anything in " " that was string.
+// boolean => standalone value // null mtlb mere ander koi bhi value nhi hai/ type object hai.
+// undefined => 
+// symbol => unique
+
+// Object
+
+console.log(typeof "Mohit"); // string hot ahi bcoz ye " " me hai 
+
+console.log(typeof null); // null apne apme ek khali jagah reserb karta hai or bolata hai me khali hu isliye type uska object ho jata hai 
+
+console.log(typeof undefined); // undefined to isse defined hi nhi kiya gya isliye indefined and reserv keyword. 
+
