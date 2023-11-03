308248beccdaefe6ef29768080ea6b887824ca71
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-11-03 20:17:36 +0000

    conversion of data few type

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -92,3 +92,29 @@ console.log(typeof valueInwithFalse);
 console.log(valueInwithFalse);
 
 /* ===================================================================== */
+
+
+// Notes //
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+/* Notes */
+
+// 1 => true; 0 => false
+// "" => false;
+// "Mohit" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
