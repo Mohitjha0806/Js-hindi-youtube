f82002a5093bbeacef283ee5276413988e8fb5dc
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-11-03 19:58:51 +0000

    check all data tyle in conversion In Number

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,94 @@
-console.log("Mohit Jha")
-console.log("Mohit Jha")
-console.log("Mohit Jha")
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* ===================================================================== */
+
+let numInString = "33";
+
+console.log(typeof numInString);
+console.log(typeof (numInString));
+
+let valuenumInString = Number(numInString);
+
+console.log(typeof valuenumInString);
+console.log(valuenumInString);
+
+/* ===================================================================== */
+
+let num = "33abc";
+
+console.log(typeof num);
+console.log(typeof (num));
+
+let valueInNum = Number(num);
+
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+/* ===================================================================== */
+
+let notNumber = "33abc";
+
+console.log(typeof notNumber);
+console.log(typeof notNumber);
+
+let valueInnotNumber = Number(notNumber);
+
+console.log(typeof valueInnotNumber);
+console.log(valueInnotNumber);
+
+/* ===================================================================== */
+
+let withNull = null;
+
+console.log(typeof withNull);
+console.log(typeof withNull);
+
+let valueInWithNull = Number(withNull);
+
+console.log(typeof valueInWithNull);
+console.log(valueInWithNull);
+
+/* ===================================================================== */
+
+let withUndefined = null;
+
+console.log(typeof withUndefined);
+console.log(typeof withUndefined);
+
+let valueInwithUndefined = Number(withUndefined);
+
+console.log(typeof valueInwithUndefined);
+console.log(valueInwithUndefined);
+
+/* ===================================================================== */
+
+let withTrue = true;
+
+console.log(typeof withTrue);
+console.log(typeof withTrue);
+
+let valueInwithTrue = Number(withTrue);
+
+console.log(typeof valueInwithTrue);
+console.log(valueInwithTrue);
+
+/* ===================================================================== */
+
+let withFalse = false;
+
+console.log(typeof withFalse);
+console.log(typeof withFalse);
+
+let valueInwithFalse = Number(withFalse);
+
+console.log(typeof valueInwithFalse);
+console.log(valueInwithFalse);
+
+/* ===================================================================== */
