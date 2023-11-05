ac17b541d62ec9a778fba243babf1e7b0d7a75c9
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-11-05 19:10:17 +0000

    icrement and decrimental opration

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,42 @@
+/* ****************Operations***************** */
+
+// let value = 3;
+// let nagValue = -value;
+
+// // console.log(nagValue)
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 / 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 % 3);
+
+// let str1 = "Mohit";
+// let str2 = " Jha";
+
+// let fullName = str1 + str2;
+
+// console.log(fullName);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(((3 + 2) * 5) % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+// console.log(num2);
+
+let gameCounter = 100;
+gameCounter++; //postFix Incrimental In Js
+++gameCounter; //Prefix Incrimental In Js
+console.log(gameCounter)
