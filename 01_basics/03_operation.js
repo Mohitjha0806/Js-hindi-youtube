730d141aef351080e17d018ff60445c3ecf919c6
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-11-07 18:43:16 +0000

    change in 03 opration

--- 01_basics/03_operation.js
+++ 01_basics/03_operation.js
@@ -1,42 +1,49 @@
 /* ****************Operations***************** */
 
-// let value = 3;
-// let nagValue = -value;
+let value = 3;
+let nagValue = -value;
 
-// // console.log(nagValue)
+console.log(nagValue)
 
-// console.log(2 + 2);
-// console.log(2 - 2);
-// console.log(2 / 2);
-// console.log(2 * 2);
-// console.log(2 ** 2);
-// console.log(2 % 3);
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 / 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 % 3);
 
-// let str1 = "Mohit";
-// let str2 = " Jha";
+let str1 = "Mohit";
+let str2 = " Jha";
 
-// let fullName = str1 + str2;
+let fullName = str1 + str2;
 
-// console.log(fullName);
+console.log(fullName);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + 2);
 
-// console.log(((3 + 2) * 5) % 3);
+console.log(((3 + 2) * 5) % 3);
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);
+console.log(+"");
 
-// let num1, num2, num3;
+let num1, num2, num3;
 
-// num1 = num2 = num3 = 2 + 2;
+num1 = num2 = num3 = 2 + 2;
 
-// console.log(num2);
+console.log(num2);
 
-let gameCounter = 100;
-gameCounter++; //postFix Incrimental In Js
-++gameCounter; //Prefix Incrimental In Js
-console.log(gameCounter)
+let gameCounterP = 100;
+gameCounterP++; //postFix Incrimental In Js
+++gameCounterP; //Prefix Incrimental In Js
+console.log(gameCounterP);
+
+
+let gameCounterN = 100;
+gameCounterN--; //postFix Decrimental In Js
+--gameCounterN; //Prefix Decrimental In Js
+console.log(gameCounterN)
