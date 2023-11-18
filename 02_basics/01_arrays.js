262d8e87321aca45d23afdbd9dbc3ae53186292e
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-11-18 16:09:55 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,40 @@
-console.log("Mohit Jha is a front end developer")
-console.log("Mohit Jha is a front end developer")
-console.log("Mohit Jha is a front end developer")
-console.log("Mohit Jha is a front end developer")
-console.log("Mohit Jha is a front end developer")
+/* Array */
+
+let myArr = [10, 12, 13, 14, 15, 16];
+console.log(myArr[3]);
+
+let myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr2[1]);
+
+let myHeros = ["Chota Bheem", "Spiderman"];
+console.log(myHeros[1]);
+
+
+/* Array Method */ 
+
+let myArr = [10, 12, 13, 14, 15, 16];
+ myArr.push(100);
+ myArr.push(200);
+ myArr.push(200);
+
+ console.log(myArr);
+
+ let myArr2 = [100, 120, 130, 140, 150, 160];
+ myArr2.pop();
+
+ console.log(myArr2);
+
+ let myArr3 = [100, 120, 130, 140, 150, 160];
+ myArr3.unshift(99);
+
+ console.log(myArr3);
+
+ let myArr4 = [100, 120, 130, 140, 150, 160];
+ myArr4.shift();
+
+ console.log(myArr4);
+
+ let myArr5 = [100, 120, 130, 140, 150, 160];
+ myArr5.shift();
+
+ console.log(myArr5.includes(130));
