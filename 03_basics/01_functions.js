fec815c9022c033614c6eb9d47311323949402d1
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-12-06 15:38:01 +0000

    function intro

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+console.log("M");
+console.log("O");
+console.log("H");
+console.log("I");
+console.log("T"); // Aam Zindagi 
+
+function sayMyName() {
+  console.log("M");
+  console.log("O");
+  console.log("H");
+  console.log("I");
+  console.log("T");
+}
+
+sayMyName(); // sayMyName = function Refrence, () = ye ho gya exicusion.  ye hai mintos zindagi 
