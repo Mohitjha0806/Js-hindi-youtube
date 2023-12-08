c04cd96cd7ee7eb46278cf1ae25109e862834307
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-12-08 10:41:48 +0000

    Changes to be committed: 	modified:   03_basics/01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,8 +1,8 @@
-// console.log("M");
-// console.log("O");
-// console.log("H");
-// console.log("I");
-// console.log("T"); // Aam Zindagi
+console.log("M");
+console.log("O");
+console.log("H");
+console.log("I");
+console.log("T"); // Aam Zindagi
 
 function sayMyName() {
   console.log("M");
@@ -12,22 +12,40 @@ function sayMyName() {
   console.log("T");
 }
 
-// sayMyName(); // sayMyName = function Refrence, () = ye ho gya exicusion.  ye hai mintos zindagi
+sayMyName(); // sayMyName = function Refrence, () = ye ho gya exicusion.  ye hai mintos zindagi
 
 function addTwoNumbers(number1, number2) {
 
-  // console.log(number1 + number2);
+  console.log(number1 + number2);
 
-  // let result = number1 + number2;
+  let result = number1 + number2;
 
-  // return result; // return likhne ke baad scope me kuch bhi likha hai vo read nhi karta hai 
-  
-  
-  return number1 + number2 //hum direct return karke bhi extra code likhne se bach sakte hai 
-  
-  // console.log("Mohit");
-}
+  return result; // return likhne ke baad scope me kuch bhi likha hai vo read nhi karta hai
+
+  return number1 + number2 //hum direct return karke bhi extra code likhne se bach sakte hai
+
+  console.log("Mohit");
+// }
 
 const result = addTwoNumbers(3, 5);
 
 console.log("Result: ", result);
+
+function loginUserMassage(username) {
+  
+  if(username === undefined){
+    console.log("Please enter a username");
+    return
+  }
+
+  if(!username){
+    console.log("Please enter a username");
+    return
+  }
+
+  return `${username} Just Logged In`;
+}
+
+console.log(loginUserMassage("Mohit"))
+
+console.log(loginUserMassage()); //jab koi value pass nhi karenge toh undefined
