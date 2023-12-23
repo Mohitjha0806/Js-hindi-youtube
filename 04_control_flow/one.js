16b8eec56a505c756af2f1b400c4ab6449a8b727
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2023-12-23 15:02:10 +0000

    control flow if else condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+// if
+
+if (true) {
+  console.log(executed); // ye condition true honi chahiye tabhi ye ander ka code execute hoga
+}
+
+if (false) {
+  console.log(executed); // ye condition false me ander ka code execute nhi hoga
+}
+
+let isLoggedIn = true
+
+if(isLoggedIn){
+    console.log("Aa Gya ander consdition true hai ")
+}
+
+let isLoggedIn = false
+
+if(isLoggedIn){
+    console.log("Aa Gya ander")
+} else{
+    console.log("Nhi hu bhai Logged In condition false ho gyi")
+}
+
+if (3 === "3") {
+  console.log("ha = Hai dono");
+} else {
+  console.log("Nhi hai = datatype alag hai");
+}
+
+if (3 == "3") {
+    console.log("ha = Hai dono");
+  } else {
+    console.log("Nhi hai = datatype alag hai");
+  }
+
+  if (3 !== "3") {
+    console.log("ha = Hai dono");
+  } else {
+    console.log("Nhi hai = datatype alag hai");
+  }
+  if (3 < 4) {
+    console.log("ha 4 bada hai ");
+  } else {
+    console.log("galat bata raha hai");
+  }
+
+  if (3 > 4) {
+    console.log("ha 4 bada hai ");
+  } else {
+    console.log("galat bata raha hai");
+  }
