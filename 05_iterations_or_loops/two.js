b8419476e086f0445fae8bdb0a16f663dca97444
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-01-08 19:35:26 +0000

    done loops

---
+++ 05_iterations_or_loops/two.js
@@ -0,0 +1,30 @@
+
+// While Loop
+
+// let index = 0;
+
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+//   index = index + 2;
+// }
+
+// let myArray = ["Superman", "Batman", "Hulk"]
+
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`)
+//     arr = arr + 1
+// }
+
+
+// Do while Loop 
+
+//do means pehle kaam hoga  while mtlb baad me condition baad me check hogi 
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
