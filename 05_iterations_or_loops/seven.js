015cd18c49643871be698ce96ed03ac6d62779be
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-01-11 15:21:08 +0000

    seven map chaining method

---
+++ 05_iterations_or_loops/seven.js
@@ -0,0 +1,36 @@
+// ============= Map ==================
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const addValue = myNumers.map((num) => num + 10); 
+
+//map ki sabse achi baat direct value return kar deta hai 
+
+console.log(addValue);
+
+const addValuee = myNumers.map((num) => {num + 10});
+
+console.log(addValuee);
+
+// scope open karne ke baad ye hume undefined return karta hai 
+
+const addValueee = myNumers.map((num) => {return num + 10});
+
+//kyoki return karna hota hai isme bhi filter ki trah
+
+console.log(addValueee);
+
+// ============== Chaining method Map Map filter sab ek ya ek se jayada baar lag jaata hai  ===================
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const add = myNums.map((num) => num + 10);
+
+const newAdd = myNums
+                    .map((num) => num*10)
+                    .map((num) => num + 1)
+                    .filter((num) => num >= 40)
+                    
+console.log(newAdd)                
+
+
