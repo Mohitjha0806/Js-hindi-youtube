1e6142b49e7ca805186f2a55165e03043477ea22
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-01-13 19:45:27 +0000

    reduce done

--- 05_iterations_or_loops/nine.js
+++ 05_iterations_or_loops/nine.js
@@ -1,8 +1,49 @@
 // ============== Reduce =============
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// (i) with fuction
 
-const myTotal = myNums.reduce(function (acc, currVal) {
-  console.log(`acc : ${acc} and currVal : ${currVal}`);
-  return acc + currVal;
-}, 0);
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const myTotal = myNums.reduce(function (acc, currVal) {
+//   console.log(`acc : ${acc} and currVal : ${currVal}`);
+//   return acc + currVal;
+// }, 0);
+
+// console.log(myTotal)
+
+// With arrow Function
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const myTotal = myNums.reduce((acc, currVal) => acc + currVal, 0);
+
+// console.log(myTotal);
+
+// Example
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 3000
+    },
+    {
+        itemName : "Python",
+        price : 3000
+    },
+    {
+        itemName : "DBMS",
+        price : 3000
+    },
+    {
+        itemName : "Web Dev",
+        price : 3000
+    },
+    {
+        itemName : "C++",
+        price : 3000
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0 );
+
+console.log(priceToPay);
