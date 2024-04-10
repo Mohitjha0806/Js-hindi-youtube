7a4a83a38f4fe9f70e3f245d7ee321e9c511254d
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-04-10 12:24:39 +0000

    Variable done

--- JS-revision/01_basics/01_variables.js
+++
@@ -1,8 +0,0 @@
-const accountId = 144553;
-let accountEmail = "mohitjha95844@gmail.com"
-let accountPassword = "mohit@0858"
-accountCity = "Bhopal"
-
-accountId = 1;
-
-console.log(accountId);
---
+++ js-revision/01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "mohitjha95844@gmail.com"
+let accountPassword = "mohit@0858"
+accountCity = "Bhopal"
+
+// accountId = 1
+accountEmail = "mohit.Jha@google.com"
+accountPassword = "0858@mohit"
+accountCity = "Indore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
