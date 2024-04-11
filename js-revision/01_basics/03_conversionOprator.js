19f90ee73fce0c089702858b192ff1c719d3f3ad
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-04-11 12:26:52 +0000

    type operator done

---
+++ js-revision/01_basics/03_conversionOprator.js
@@ -0,0 +1,61 @@
+let score = 33;
+
+console.log(typeof score)
+
+// huw to conver string value to number
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+ 
+let score3 = "33abc";
+
+console.log(typeof score3)
+
+let valueInNumber3 = Number(score3);
+console.log(typeof valueInNumber3)
+console.log(valueInNumber3)
+
+
+let score4 = null;
+
+console.log(typeof score4)
+
+let valueInNumber4 = Number(score4);
+console.log(typeof valueInNumber4)
+console.log(valueInNumber4)
+
+
+let score5 = undefined;
+
+console.log(typeof score5)
+
+let valueInNumber5 = Number(score5);
+console.log(typeof valueInNumber5)
+console.log(valueInNumber5)
+
+
+let score6 = true;
+
+console.log(typeof score6)
+
+let valueInNumber6 = Number(score6);
+console.log(typeof valueInNumber6)
+console.log(valueInNumber6)
+
+let score7 = false;
+
+console.log(typeof score7)
+
+let valueInNumber7 = Number(score7);
+console.log(typeof valueInNumber7)
+console.log(valueInNumber7)
+
+
+let score8 = "Mohit";
+
+console.log(typeof score8)
+
+let valueInNumber8 = Number(score8);
+console.log(typeof valueInNumber8)
+console.log(valueInNumber8)
