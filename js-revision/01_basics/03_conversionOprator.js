64fc672143909e67d9151fb09ec3dbb1483ad9e1
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-04-11 14:02:09 +0000

    js- 01/04 all done

--- js-revision/01_basics/03_conversionOprator.js
+++ js-revision/01_basics/03_conversionOprator.js
@@ -1,61 +1,112 @@
-let score = 33;
+// let score = 33;
 
-console.log(typeof score)
+// console.log(typeof score)
 
-// huw to conver string value to number
+// // huw to conver string value to number
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
- 
-let score3 = "33abc";
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-console.log(typeof score3)
+// let score3 = "33abc";
 
-let valueInNumber3 = Number(score3);
-console.log(typeof valueInNumber3)
-console.log(valueInNumber3)
+// console.log(typeof score3)
 
-
-let score4 = null;
-
-console.log(typeof score4)
-
-let valueInNumber4 = Number(score4);
-console.log(typeof valueInNumber4)
-console.log(valueInNumber4)
+// let valueInNumber3 = Number(score3);
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)
 
 
-let score5 = undefined;
+// let score4 = null;
 
-console.log(typeof score5)
+// console.log(typeof score4)
 
-let valueInNumber5 = Number(score5);
-console.log(typeof valueInNumber5)
-console.log(valueInNumber5)
-
-
-let score6 = true;
-
-console.log(typeof score6)
-
-let valueInNumber6 = Number(score6);
-console.log(typeof valueInNumber6)
-console.log(valueInNumber6)
-
-let score7 = false;
-
-console.log(typeof score7)
-
-let valueInNumber7 = Number(score7);
-console.log(typeof valueInNumber7)
-console.log(valueInNumber7)
+// let valueInNumber4 = Number(score4);
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4)
 
 
-let score8 = "Mohit";
+// let score5 = undefined;
 
-console.log(typeof score8)
+// console.log(typeof score5)
 
-let valueInNumber8 = Number(score8);
-console.log(typeof valueInNumber8)
-console.log(valueInNumber8)
+// let valueInNumber5 = Number(score5);
+// console.log(typeof valueInNumber5)
+// console.log(valueInNumber5)
+
+
+// let score6 = true;
+
+// console.log(typeof score6)
+
+// let valueInNumber6 = Number(score6);
+// console.log(typeof valueInNumber6)
+// console.log(valueInNumber6)
+
+// let score7 = false;
+
+// console.log(typeof score7)
+
+// let valueInNumber7 = Number(score7);
+// console.log(typeof valueInNumber7)
+// console.log(valueInNumber7)
+
+
+// let score8 = "Mohit";
+
+// console.log(typeof score8)
+
+// let valueInNumber8 = Number(score8);
+// console.log(typeof valueInNumber8)
+// console.log(valueInNumber8)
+
+
+// <====================== Oprations ===========================>
+
+// let value = 3;
+// let nagative = -value
+// // console.log(nagative)
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(10 / 2);
+// console.log(10 % 200);
+
+
+// let firstName = "Mohit"
+// let lastName = "Jha"
+
+// console.log(`${firstName}`, `${lastName}`)
+
+
+// let str1 = "Mohit"
+// let str2 = " Jha"
+
+// let str3 = str1 + str2;
+
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 5)
+
+// console.log(+true);
+// console.log(+"");
+
+// let = num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100
+
+// gameCounter++
+++gameCounter
+
+console.log(gameCounter)    
+
+
