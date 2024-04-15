c0f2eec98c487a3aba119bb2f70c04d5f71ae90b
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-04-15 09:26:43 +0000

    Data types of javascript summary

---
+++ js-revision/01_basics/datatype_summary.js
@@ -0,0 +1,38 @@
+// Primitive 
+
+// 7 types: - All are call by value
+
+// 1. String
+// 2. Boolean
+// 3. null
+// 4. undefined
+// 5. Number
+// 6. Symbol
+// 7. BigInt
+
+// ****************************************************************
+
+// Non-Primitivre( Refrence type )
+
+// 3 Type: - 
+
+// 1. Array
+// 2. Object
+// 3. Function
+
+// ****************************************************************
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null; // have No temprature - 0 + Nothing
+
+let userEmail; // Undefined
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(id == anotherId); // False // : - Symbol Example
+
+const bigNumber = 897645312231654456n;
