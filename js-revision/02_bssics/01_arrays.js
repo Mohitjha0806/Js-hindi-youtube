05546354971a80a3669d60d97612d1da02077118
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-04-19 11:38:45 +0000

    modified:   js-revision/02_bssics/01_arrays.js

--- js-revision/02_bssics/01_arrays.js
+++ js-revision/02_bssics/01_arrays.js
@@ -1,34 +1,34 @@
 // Array
 
 const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const myHeros = ["MSD", "Virat", "Hitman",];
+const myHeros = ["MSD", "Virat", "Hitman",];
 
-// const myArr2 = new Array([1, 2, 3, 4, 5])
+const myArr2 = new Array([1, 2, 3, 4, 5])
 
-// console.log(myArr[8]);
+console.log(myArr[8]);
 
 // Array Method 
 
-// myArr.push(6)
-// myArr.push(7) // This Method add last Elemet of Array.
+myArr.push(6)
+myArr.push(7) // This Method add last Elemet of Array.
 
-// myArr.pop() // This Method Remove Last Elemet of Array.
-// myArr.unshift(111) // This Method add Elemet in a Start of Array because of witch indexing of array goes wrong.
-// myArr.shift() // This Method remove Elemet in a Start of Array because of witch indexing of array goes wrong.
+myArr.pop() // This Method Remove Last Elemet of Array.
+myArr.unshift(111) // This Method add Elemet in a Start of Array because of witch indexing of array goes wrong.
+myArr.shift() // This Method remove Elemet in a Start of Array because of witch indexing of array goes wrong.
 
 // Question & Answer method
 
-// const isYesOrNot = myArr.includes(6)
-// console.log(isYesOrNot)
+const isYesOrNot = myArr.includes(6)
+console.log(isYesOrNot)
 
-// console.log(myArr.indexOf(13))
+console.log(myArr.indexOf(13))
 
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
-// console.log(myArr)
-// console.log(newArr)
-// console.log(typeof newArr)
+console.log(myArr)
+console.log(newArr)
+console.log(typeof newArr)
 
 
 // Slice & Splice
